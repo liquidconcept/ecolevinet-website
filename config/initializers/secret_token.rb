bee232e280116f613ce89601ded36da38b5aeacd
nicolas couturier <nicolas.couturier@liquid-concept.ch>
2011-08-29 18:30:09 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EcolevinetWebsite::Application.config.secret_token = 'f5d13ff68ba585acd540db8002a3a9a3e0a5b9ee1b908e232ee647d58bf5248d3cc15d96bebbdd1f7912f0eb90d855ed1ffedd49b744ecb839f37bcbad0ab863'
