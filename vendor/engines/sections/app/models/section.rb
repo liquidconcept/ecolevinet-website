22756a3e070dc3d1f37141a8f478bd03f964771c
nicolas couturier <nicolas.couturier@liquid-concept.ch>
2011-09-14 13:08:03 +0200

    modified views for portfolio & sections

--- vendor/engines/sections/app/models/section.rb
+++ vendor/engines/sections/app/models/section.rb
@@ -4,7 +4,6 @@ class Section < ActiveRecord::Base
 
   validates :nom,   :presence => true, :uniqueness => true
   validates :titre,   :presence => true
-  validates :chapeau, :presence => true
   validates :lien, :format => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix, :allow_blank => true
 
   belongs_to :image
