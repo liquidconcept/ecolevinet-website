62a16fc78124132c409e6e13d756aa1cbc7b08bd
nicolas couturier <nicolas.couturier@liquid-concept.ch>
2011-10-05 10:12:24 +0200

    modified calendar view and associated helper

--- app/helpers/page_helper.rb
+++ app/helpers/page_helper.rb
@@ -11,19 +11,21 @@ module PageHelper
   def current_day_number
     l(current_day , :format => :day_number)
   end
-  def first_day
-    Date.today.beginning_of_month.beginning_of_week
+  def first_day(day)
+    day.to_date.beginning_of_month.beginning_of_week
   end
-  def last_day
-    Date.today.end_of_month.end_of_week
+  def last_day(day)
+    day.to_date.end_of_month.end_of_week
   end
   def day_type(day)
     klass =  ''
-    klass << ' first'       if day.to_date == day.beginning_of_month.to_date
+    klass << ' first'       if day.to_date.cwday == 1
     klass << ' weekend'     if day.to_date.cwday >= 6
     klass << ' event_check' if Event.given_day(day).count > 0
     klass
   end
-
+  def week_type(day)
+    day.to_date.cweek == current_day.cweek ? 'id="current_week"' : ''
+  end
 
 end
