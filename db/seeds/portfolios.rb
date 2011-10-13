b822304bd618d8bebe1daac53f7afbabf273489c
nicolas couturier <nicolas.couturier@liquid-concept.ch>
2011-10-13 16:26:16 +0200

    modified portfolio seeds to populate data on pre existing sections

--- db/seeds/portfolios.rb
+++ db/seeds/portfolios.rb
@@ -13,3 +13,24 @@ if defined?(Page) && !Page.find_by_link_url('/portfolio').present? && !RefineryC
     page.parts.create(:title => default_page_part, :body => nil)
   end
 end
+
+
+if defined?(Section)  && !RefineryConfig.hidden_plugins.include?("portfolio")
+
+  Section.all.each do |section|
+    if section.page.children.select {|p| p.data_type == 'portfolio'}.blank?
+
+      page = section.page.children.create(
+        :title => 'Galerie',
+        :deletable => false,
+        :show_in_menu => true,
+        :data_type => 'portfolio',
+        :position => section.page.position + 1)
+        Page.default_parts.each do |default_page_part|
+          page.parts.create(:title => default_page_part, :body => nil)
+        end
+
+    end
+  end
+
+end
