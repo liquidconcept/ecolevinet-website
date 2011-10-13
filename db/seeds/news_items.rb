857f55c8aaaec4ddcabd868f83582d660d4b9721
nicolas couturier <nicolas.couturier@liquid-concept.ch>
2011-10-13 18:50:52 +0200

    added news_items page creation for pre existing sections

--- db/seeds/news_items.rb
+++ db/seeds/news_items.rb
@@ -12,3 +12,22 @@ if defined?(Page) && !Page.find_by_link_url('/news_items').present? && !Refinery
   end
 
 end
+
+
+if defined?(Page) && defined?(Section) && !RefineryConfig.hidden_plugins.include?("news_items")
+
+  Section.all.each do |section|
+    if section.page.children.select{|c| c.data_type == 'news_items'}.blank?
+      page = section.page.children.create(
+        :title => 'Actualités',
+        :deletable => false,
+        :show_in_menu => true,
+        :data_type => 'news_items',
+        :position => section.page.position + 1)
+        Page.default_parts.each do |default_page_part|
+          page.parts.create(:title => default_page_part, :body => nil)
+        end
+    end
+  end
+
+end
