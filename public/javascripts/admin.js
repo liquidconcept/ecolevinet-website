abf51beef9449601e206008b32d5315f724dbd78
nicolas couturier <nicolas.couturier@liquid-concept.ch>
2011-10-27 10:50:43 +0200

    suppressed use of h1

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -3,5 +3,10 @@
 // This will completely override anything specified in boot_wym.js for that key.
 // e.g. skin: 'something_else'
 var custom_wymeditor_boot_options = {
-
+   containersItems: [
+     {'name': 'h2', 'title':'Heading_2', 'css':'wym_containers_h2'}
+    , {'name': 'h3', 'title':'Heading_3', 'css':'wym_containers_h3'}
+    , {'name': 'h4', 'title':'Heading_4', 'css':'wym_containers_h4'}
+    , {'name': 'p', 'title':'Paragraph', 'css':'wym_containers_p'}
+  ]
 };
