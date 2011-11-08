7b70f62d668d0d532dfd291396e4e7ccd5cea014
nicolas couturier <nicolas.couturier@liquid-concept.ch>
2011-11-08 15:36:21 +0100

    minor correction to page served to agenda

--- app/controllers/agenda_controller.rb
+++ app/controllers/agenda_controller.rb
@@ -1,6 +1,6 @@
 class AgendaController < ApplicationController
   def index
     @events = Event.where('start_date >= ?',Date.today).order("start_date DESC")
-    @page   = Page.find_by_link_url('/')
+    @page   = Page.find_by_link_url('/agenda/index')
   end
 end
