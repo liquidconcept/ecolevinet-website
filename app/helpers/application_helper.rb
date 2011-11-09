6ad42567c4f55781d7288cd5797d9b8b4e966eaa
Yann Lugrin <yann.lugrin@liquid-concept.ch>
2011-11-09 10:54:20 +0100

    breadcrumb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def breadcrumb(page, html = '')
+    return html unless page
+
+    html = link_to(page.title, url_for(page)) + ' / ' + html
+    breadcrumb(page.parent, html).html_safe
+  end
 end
